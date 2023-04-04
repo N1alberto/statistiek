610458145f62e87808600ea5b1eaafb0999a45d4
N1alberto <112620933+N1alberto@users.noreply.github.com>
2023-04-04 10:27:41 +0200

    Javascript
    Opdracht 1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("main.js loaded 🎉");
+
+const numbers = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+for (let i = 0; i < numbers.length; i++) {
+    numbersAmount = 13;
+    numbersArray = ([numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9] + numbers[10] + numbers[11] + numbers[12] ]);
+    
+}
