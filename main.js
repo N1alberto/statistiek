693cc61d96d1fdb7e32e371fe670d9b15456330e
N1alberto <112620933+N1alberto@users.noreply.github.com>
2023-04-04 11:29:59 +0200

    Opdracht 1 en 2
    De rest begrijp ik niet

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ console.log("main.js loaded 🎉");
 // }
 
 
-const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
-const devided = numbers.length;
-let answer = 0;
-for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i]
-    answer = number/devided;
+// const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
+// const devided = numbers.length;
+// let answer = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i]
+//     answer = number/devided;
     
-}
-document.body.innerHTML += answer;
+// }
+// document.body.innerHTML += answer;
+
