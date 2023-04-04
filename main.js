141ff160b213eeae4334899e860c1346b3481cde
N1alberto <112620933+N1alberto@users.noreply.github.com>
2023-04-04 11:17:20 +0200

    Opdracht 2 klaar
    Door naar opdracht 3

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 console.log("main.js loaded 🎉");
 
-const numbers = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+// const numbers = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+// const devided = numbers.length;
+// let answer = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i]
+//     const answer = number/devided;
+//     console.log(answer);
+// }
+
+
+const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
 const devided = numbers.length;
+let answer = 0;
 for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[1]
-    const answer = number/devided;
-    console.log(answer);
+    const number = numbers[i]
+    answer = number/devided;
+    
 }
+document.body.innerHTML += answer;
