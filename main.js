30170199db7cb37885a9ecd1235f445ab9c62d80
N1alberto <112620933+N1alberto@users.noreply.github.com>
2023-04-11 10:07:28 +0200

    Klaar
    Niet gespiekt ofzo

--- main.js
+++ main.js
@@ -1,22 +1,78 @@
 console.log("main.js loaded 🎉");
 
+// Opdracht 1
 // const numbers = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+// let answer = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i]
+//     answer += number;
+// }
+
+// answer = answer/numbers.length;
+// console.log(Math.floor(answer));
+
+// ***********************************************************************************************************
+
+// Opdracht 2
+// const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
 // const devided = numbers.length;
 // let answer = 0;
 // for (let i = 0; i < numbers.length; i++) {
 //     const number = numbers[i]
-//     const answer = number/devided;
-//     console.log(answer);
+//     answer = number/devided;
+    
+// }
+// document.body.innerHTML += "Afgerond naar boven " + Math.ceil(answer) +"<br>";
+// document.body.innerHTML += "Afgerond naar beneden " + Math.floor(answer);
+
+// ***********************************************************************************************************
+
+// Opdracht 3
+// const numbers = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+// let highestNumber = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+    
+//     if (highestNumber < number) {
+//         highestNumber = number
+//     }
+// }
+// console.log(highestNumber)
+
+// ***********************************************************************************************************
+
+// Opdracht 4
+// const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
+// let lowestNumber = 10000;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+    
+//     if (lowestNumber > number) {
+//         lowestNumber = number
+//     }
+// }
+// console.log(lowestNumber)
+
+// ***********************************************************************************************************
+
+// Opdracht 5
+// const under30 = [15,14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+
+// for (let i = 0; i < under30.length; i++) {
+//     const number = under30[i];
+//     if (number < 30) {
+//         console.log(number)
+//     }
 // }
 
+// ***********************************************************************************************************
 
-const numbers = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
-const devided = numbers.length;
-let answer = 0;
-for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i]
-    answer = number/devided;
-    
-}
-document.body.innerHTML += answer;
+// Opdracht 6
+// const above200 = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
 
+// for (let i = 0; i < above200.length; i++) {
+//     const number = above200[i];
+//     if (number > 200) {
+//         console.log(number)
+//     }
+// }
